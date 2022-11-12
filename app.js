8c25cebc0b946656bcf05f3f1db9a07c7d1bd3f7
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-12 19:56:41 +0500

    alphabet

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ var num2 = 60;
 var num3 = 0.5;
 var next2 = "Siz " + (num2 - num) * 365 * num3 + " l choy ichasiz";
 console.log(next2); //Siz 8212.5 l choy ichasiz
+
+function alphabet_order(str) {
+  return str.split("").sort().join("");
+}
+console.log(alphabet_order("Mushtariy"));
