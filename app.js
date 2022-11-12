3cc24a24750476e649749fb2133ceda6a75aaadd
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-12 20:39:46 +0500

    lang

--- app.js
+++ app.js
@@ -11,7 +11,49 @@ var num3 = 0.5;
 var next2 = "Siz " + (num2 - num) * 365 * num3 + " l choy ichasiz";
 console.log(next2); //Siz 8212.5 l choy ichasiz
 
+//alphabet
 function alphabet_order(str) {
   return str.split("").sort().join("");
 }
 console.log(alphabet_order("Mushtariy"));
+
+//. /[l]/g
+let text = "Hello world! My friends";
+let search = /[l]/g;
+let result = text.match(search);
+console.log(result);
+
+//Dunyo tarjimoni
+function helloWorld(lang) {
+  if (lang == "fr") {
+    return "Bonjour";
+  } else if (lang == "cn") {
+    return "你好呀";
+  } else if (lang == "en") {
+    return "Hello";
+  } else if (lang == "ks") {
+    return "안녕하세요";
+  } else if (lang == "as") {
+    return "أهلاً";
+  } else if (lang == "ts") {
+    return "сәлам анда";
+  } else if (lang == "af") {
+    return "haai daar";
+  } else if (lang == "qg") {
+    return "Салам";
+  } else if (lang == "fr") {
+    return "Bonjour à tous";
+  } else if (lang == "ru") {
+    return "Всем привет";
+  }
+}
+console.log(helloWorld("fr"));
+console.log(helloWorld("cn"));
+console.log(helloWorld("en"));
+console.log(helloWorld("ks"));
+console.log(helloWorld("as"));
+console.log(helloWorld("ts"));
+console.log(helloWorld("af"));
+console.log(helloWorld("qg"));
+console.log(helloWorld("fr"));
+console.log(helloWorld("ru"));
