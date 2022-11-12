78ed2f0f5c351535a8be23071e2e3d42ee009ebb
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-12 18:28:31 +0500

    12.11.22

---
+++ app.js
@@ -0,0 +1,12 @@
+// Yosh kakulyatori
+var year = 2022;
+var year2 = 1975;
+var next = "Sizning yoshingiz " + (year - year2) + " da"; //Sizning yoshingiz 47 da
+console.log(next);
+
+// Choy qancha istemol qilish hisobi
+var num = 15;
+var num2 = 60;
+var num3 = 0.5;
+var next2 = "Siz " + (num2 - num) * 365 * num3 + " l choy ichasiz";
+console.log(next2); //Siz 8212.5 l choy ichasiz
