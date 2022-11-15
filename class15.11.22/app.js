0d2282d9b9b93e79410cb8e8d896d84af3d2aac7
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-15 20:01:49 +0500

    new

---
+++ class15.11.22/app.js
@@ -0,0 +1,14 @@
+const signupForm = document.querySelector("#sinupup-form");
+const message = document.querySelector(".message");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const RegEx = /^[a-zA-Z0-9$]{6,12}/;
+  const inputVal = signupForm.username.value;
+
+  if (RegEx.test(input)) {
+    message.textContent = "Ma'lumot to'g'ri";
+  } else {
+    message.textContent =
+      "Xatolik aniqlandi.Kiritayotgan ma'lumotda 6 va 12 oralig'ida bo'lsin";
+  }
+});
