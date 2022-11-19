fcf401061f8ca765cd38b22a83733355e277ff04
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-19 19:25:29 +0500

    grid

---
+++ 17.11.22/app.js
@@ -0,0 +1,13 @@
+let tabs = document.querySelectorAll(".tabs h3")
+let tabContainer = document.querySelectorAll(".tab-container div")
+
+.tabs.forEach()
+
+
+
+
+
+console.log("Default %cPink %cBlack", 
+"color: hotpink; font-size: 2em;"
+'color:black; font-size: 3em;'
+);
